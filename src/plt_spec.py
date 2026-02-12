82ad50d2419d7c3f9ef0a1b925be9375b1c35fc2
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2026-02-12 16:55:15 -0500

    Remove subplots sharey. Generally make things more legible.

--- src/plt_spec.py
+++ src/plt_spec.py
@@ -19,38 +19,28 @@ def plt_prefs():
     plt.rc('axes', labelsize='small')
     plt.rc('lines', markerfacecolor='none')    ## always non-filled markers
 
-# given year and month, return epoch number
 def epoch(year, month):
-    # first epoch started in 2000-02
+    '''
+    Given year and month, return epoch number.
+
+    The first epoch began 2000-02.
+    '''
+
     nmonths=(int(year)-2000)*12 + month-2
     return int(nmonths/3)+1
 
-# given epoch number, return year and month of beginning
 def epoch_start(epoch):
+    '''Given epoch number, return year and month it began.'''
     nmonths = (epoch-1)*3
     year = 2000+int(nmonths/12)
     month = 2+nmonths%12
     return year, month
 
-xtra=''; xtra_label='CALDB'
-#xtra='_v772025'; xtra_label='v772025'
-#xtra='_v7232025'; xtra_label='v7232025'
-#xtra='_v7242025'; xtra_label='v7242025'
-#xtra='_v7252025'; xtra_label='v7252025'
-
 def mkplot(args, data, ccd):
-
-    f,ax= plt.subplots(4,1,sharex=1,sharey=1)
+    f,ax= plt.subplots(4,1,sharex=1)
     f.canvas.manager.set_window_title(ccd.upper())
-    my_lef=0.1; my_rt=0.95; my_bot=0.08; my_top=0.93; my_hs=0; my_ws=0.2
-    f.subplots_adjust(left=my_lef, right=my_rt, bottom=my_bot, top=my_top, hspace=my_hs, wspace=my_ws)
+    f.subplots_adjust(left=0.1, right=0.95, bottom=0.08, top=0.93, hspace=0)
     c=['cyan','red','orange','lime']; fmt=['+','o','s','d']
-
-    ylim = {
-        'Al':(1.40, 1.53),
-        'Ti':(4.41, 4.61),
-        'Ml':(5.75, 5.95),
-    }.get(args.line)
 
     td15= timedelta(days=5)
     td05= timedelta(days=2.5)
@@ -75,23 +65,16 @@ def mkplot(args, data, ccd):
 
     global nom
     for i in range(0,4):
-        lbl1='+- 0.5%' if i==0 else ''
-        ax[i].axhline(0,ls='--',lw=0.5,c='gray',label=lbl1)
-        ax[i].axhline(-1e3*0.005*nom,ls='--',lw=0.5,c='gray')
-        ax[i].axhline(1e3*0.005*nom,ls='--',lw=0.5,c='gray') 
+        label = r'$\pm 0.5\%$' if i==0 else None
+        opts = {'ls':'--', 'lw':0.5, 'c':'gray'}
+        ax[i].axhline(0, **opts, label=label)
+        ax[i].axhline(-1e3*0.005*nom, **opts)
+        ax[i].axhline( 1e3*0.005*nom, **opts)
 
-    vfact=0.015 if args.line=='Mn' else 0.03
-    vmin= nom - vfact*nom; vmax= nom + vfact*nom
-    vmin = {
-        'Al':-40,
-        'Ti':-80,
-        'Mn':-80,
-    }.get(args.line)
-    vmax = {
-        'Al':40,
-        'Ti':80,
-        'Mn':80,
-    }.get(args.line)
+    xlo, xhi = ax[0].get_xlim()
+
+    vmax = 80
+    vmin = -vmax
 
     vlines = ['2021-11-01', '2022-09-15', '2023-03-01', '2023-12-15', '2024-10-15']
 
@@ -102,27 +85,17 @@ def mkplot(args, data, ccd):
                 label='tgain begins'
             ax[i].vlines(datetime.strptime(date,'%Y-%m-%d'),vmin,vmax,color='blue',label=label,lw=0.5,ls='--')
 
+    ax[0].set_xlim(xlo, xhi)
+
     global temps
     f.suptitle(f'ECS {args.line}-Ka LineE -{temps.max()}.19:-{temps.min()-1}.19C -- {ccd.upper()} -- 256x256y \t\t{ecsid}'.expandtabs())
     f.supylabel('LineE shift [eV]')
     for i in range(4):
         ax[i].set_ylabel(f'node{i}')
 
-    #ax[0].set_xlim(datetime.strptime('2020-10-01',dfmt), datetime.strptime('2025-05-01',dfmt))
-
-    ylim = {
-        'Al':[-50,50],
-        'Ti':[-100,100],
-        'Mn':[-100,100],
-    }.get(args.line)
-
-    #ax[0].set_ylim(ylim)
-    z=ax[0].get_yticks()   ##; print(z)
-    newz=[z[1],z[2],z[3]]; ax[0].set_yticks(newz)
     ax[0].legend()
     ax[1].legend(loc='lower left', frameon=False)
 
-    plt.tight_layout()
     return f
 
 def year_mid_epoch(e):
@@ -159,14 +132,13 @@ def plt_spec(args):
 
         for i, e in enumerate(args.epochs):
             inf=f'{datadir}/e{e:03d}/fits/{ecsid}/fits/fpt_{fpt}_256x256y/{ccd}_ecs.txt'
+            lineE_col = { 'Al':4,
+                          'Ti':9,
+                          'Mn':19,
+                         }.get(args.line)
             try:
-                (xl,yl,al,al_lo,al_hi,ti,ti_lo,ti_hi,mn,mn_lo,mn_hi,stat)= np.loadtxt(inf, skiprows=2, unpack=1, usecols=[0,2,4,5,6,9,10,11,19,20,21,-1])
-
-                li, li_lo, li_hi = {
-                    'Al':(al, al_lo, al_hi),
-                    'Ti':(ti, ti_lo, ti_hi),
-                    'Mn':(mn, mn_lo, mn_hi),
-                }.get(args.line)
+                usecols = [0,2] + list(range(lineE_col, lineE_col+3)) + [-1]
+                (xl,yl,li,li_lo,li_hi,stat)= np.loadtxt(inf, skiprows=2, unpack=1, usecols=usecols)
 
                 if (len(xl) != 16):
                     raise RuntimeError(f'len(xl)=f{len(xl)} in f{inf}')
@@ -191,6 +163,7 @@ def plt_spec(args):
         d['years'] = years
 
         fig = mkplot(args, d, ccd)
+        plt.tight_layout()
         if args.pdf:
             pdf.savefig(fig)
 
